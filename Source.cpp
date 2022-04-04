b39172171eceb8b003863ee8057635ec33754b76
virus242 <vadimryzhih@gmail.com>
2022-04-04 17:31:15 +0300

    test

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+bool chek(int x) {
+	return (x % 2 == 0);
+
+}
+
+int main(){
+	int x;
+	std::cin >> x;
+	if (chek(x) == 1 && x!=2)std::cout << "YES";
+	else std::cout << "NO";
+	
+}
+
+
